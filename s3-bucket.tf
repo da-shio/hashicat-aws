6e1ee619eb9de9e728ff871a0130631d5e2b86ff
da-shio <souru.mtx@gmail.com>
2022-03-02 07:49:55 +0000

    Added s3 bucket module 1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/ynagami-training/s3-bucket/aws"
+  source  = "app.terraform.io/dshioda//s3-bucket/aws"
   version = "2.2.0"
   bucket_prefix = "${var.prefix}"
   # insert required variables here
